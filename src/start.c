d02996e94a1b339e9cdee1bf20c709eed86d56eb
Jinan Harb <138831150+jinanharb@users.noreply.github.com>
2025-12-04 10:33:59 +0100

    Update start.c

--- src/start.c
+++ src/start.c
@@ -1,30 +1,80 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <cpu.h>
+#include "processus.h"
+#include "timer.h"
 
-/* on peut s'entrainer a utiliser GDB avec ce code de base */
-/* par exemple afficher les valeurs de n et res avec la commande display */
-/* une fonction bien connue */
-uint32_t fact(uint32_t n)
+// Fonctions de test des processus
+void idle(void) 
 {
-    uint32_t res;
-    if (n <= 1) {
-        res = 1;
-    } else {
-        res = fact(n - 1) * n;
+    for (;;) 
+    {
+        printf("[%s] pid = %i\n", mon_nom(), mon_pid());
+        enable_it();
+        hlt();
+        disable_it();
     }
-    return res;
 }
 
-void kernel_start(void)
-{
-
-    uint32_t x = fact(5);
-    /* quand on saura gerer l'ecran, on pourra afficher x */
-    (void)x;
-    /* on ne doit jamais sortir de kernel_start */
-    while (1) {
-        /* cette fonction arrete le processeur */
+void proc1(void) {
+    for (;;) {
+        printf("[%s] pid = %i\n", mon_nom(), mon_pid());
+        enable_it();
         hlt();
+        disable_it();
     }
+}
+
+void proc2(void) {
+    for (;;) {
+        printf("[%s] pid = %i\n", mon_nom(), mon_pid());
+        enable_it();
+        hlt();
+        disable_it();
+    }
+}
+
+void proc3(void) {
+    for (;;) {
+        printf("[%s] pid = %i\n", mon_nom(), mon_pid());
+        enable_it();
+        hlt();
+        disable_it();
+    }
+}
+
+
+void kernel_start(void) {
+
+    //enable_it();
+    init_traitant_timer(mon_traitant);
+    enable_timer();
+    //disable_it();
+
+    efface_ecran();
+
+    // Initialize process
+    init_proc();
+    int32_t pid = mon_pid();
+    char *nom = mon_nom();
+    printf("Processus actif PID: %d\n", pid);
+    printf("Process actif nom: %s\n\n", nom);
+
+    printf("creation de proc1\n");
+    pid = cree_processus(proc1, "proc1");
+    ordonnance();
+    printf("Processus actif PID: %d\n\n", pid);
+    
+    printf("creation de proc2\n");
+    pid = cree_processus(proc2, "proc2");
+    printf("Processus actif PID: %d\n\n", pid);
+
+    printf("creation de proc3\n");
+    pid = cree_processus(proc3, "proc3");
+    printf("Processus actif PID: %d\n\n", pid);
+
+    idle();
+
+    printf("Tests Done\n");
+
 }
