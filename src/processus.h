e3cfbc871f6ef2391c7d56d36753c687a99d7234
Jinan Harb <138831150+jinanharb@users.noreply.github.com>
2025-12-16 10:59:02 +0100

    Update processus.h

--- src/processus.h
+++ src/processus.h
@@ -1,41 +1,48 @@
-#ifndef __PROCESSUS_H__
-#define __PROCESSUS_H__
+#ifndef PROCESSUS_H
+#define PROCESSUS_H
 
 #include <inttypes.h>
-#include "timer.h"
+#include <string.h>
 
-#define NBPROC 10
-#define NOM_PROC_MAX 20
-#define STACK_SIZE 4096
-#define NB_REGISTRES 17
-
+#define MAX_PROCESSES 6
+#define PROCESS_STACK_SIZE 4096
+#define PROCESS_NAME_LEN 32
+#define CONTEXT_SIZE 18
 
 typedef enum {
     ELU,
     ACTIVABLE,
     ENDORMI,
     MORT
-} etat_t;
+} PROCESS_STATE;
 
-// Structure de processus
-typedef struct processus {
+typedef struct {
+    uint64_t regs[CONTEXT_SIZE];
+} context_t;
+
+typedef struct {
     int32_t pid;
-    char nom[NOM_PROC_MAX];
-    etat_t etat;
-    uint64_t registres[NB_REGISTRES];
-    uint64_t pile[STACK_SIZE];
-    uint64_t reveil;
-} processus_t;
+    char name[PROCESS_NAME_LEN];
+    PROCESS_STATE state;
+    context_t context;
+    uint64_t stack[PROCESS_STACK_SIZE];
+    uint64_t wakeup_time;
+} process_t;
 
-
-void init_proc(void);
-int32_t cree_processus(void (*)(void), char *);
-void ordonnance(void);
-int32_t mon_pid(void);
-char *mon_nom(void);
-void dors(uint32_t);
+extern void init_proc(void);
+extern int32_t cree_processus(void (*f)(void), const char *name);
+extern void ordonnance(void);
+extern int64_t mon_pid(void);
+extern char *mon_nom(void);
+void idle(void);
+void proc1(void);
+void proc2(void);
+void proc3(void);
+void dors(uint64_t nbr_secs);
+void proc_launcher(void (*proc)(void));
 void fin_processus(void);
 
-extern void ctx_sw(uint64_t *ancien_contexte, uint64_t *nouveau_contexte);
 
-#endif 
+extern void ctx_sw(uint64_t *ctxt_de_sauvegarde, uint64_t *ctxt_a_restaurer);
+
+#endif
